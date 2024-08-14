f7822a1d76c7fd966725d42af59e78a0e1b9b9d5
Eric Busboom <eric@civicknowledge.com>
2024-08-14 12:53:58 -0700

    .

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,3 +4,9 @@ pip3 install --upgrade pip
 pip3 install  -r requirements.txt
 
 git config --global pull.rebase true
+
+echo "export PYTHONPATH=$(pwd)/lib/:$PYTHONPATH" >> ~/.zshrc
+echo "export PYTHONPATH=$(pwd)/lib/:$PYTHONPATH" >> ~/.bashrc
+echo "export PYTHONPATH=$(pwd)/lib/:$PYTHONPATH" >> ~/.profile
+
+source ~/.bashrc
