c4d5a6efbd677e51a2cc1419c7f8688796112dad
Eric Busboom <eric@civicknowledge.com>
2024-08-16 20:39:44 -0700

    More proofreading

---
+++ lessons/02_Loops/04b_List_Story.py
@@ -0,0 +1,17 @@
+"""
+Use the words in the list to create a story. 
+
+Use indexing to get words from the list, then 
+append them to the story
+
+"""
+
+words = ['Once', '👦', 'upon', '🐕', 'park', 'met', 'with', 'a', 'the', 
+    'time', 'to', 'who', '🐈', '👧', 'and', 'went', 'had', 'play', '⚽.', 'they']
+
+story = []
+
+# Create a story using the words in the list
+
+# Display the story to the user
+print(' '.join(story))
