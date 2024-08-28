64efdc4abf7c795e8e20abdeec5c6abdf4e146c7
Eric Busboom <eric@civicknowledge.com>
2024-08-27 21:39:35 -0700

    renaming

--- lessons/00_Turtles/02_Meet_Tina.py
+++ lessons/00_Turtles/02_Meet_Tina.py
@@ -1,7 +1,14 @@
-# Here is a simple Turtle program that draws a square and writes a message.
-# The lines that start with a # are comments. They are not executed by Python.
-# Comments are used to explain what the code does.
-# Read the program and try to understand what each line does.
+"""
+The is a simple Turtle program that draws a square and writes a message. The
+lines that start with a # are comments. They are not executed by Python. The
+lines inside the three quotes are also comments, but of a different sort (
+called "doc comments" ) Comments are used to explain what the code does. Read
+the program and try to understand what each line does.
+
+RUM ME! YOu can run this program by clicking on ▶️ icon ar the top of the editor
+window.
+
+"""
 
 import turtle                           # Tell Python we want to work with the turtle
 turtle.setup(width=600, height=600)     # Set the size of the window
--- lessons/00_Turtles/04a_Turtle_Tricks.py
+++ lessons/00_Turtles/04a_Turtle_Tricks.py
@@ -1,8 +1,8 @@
 """
 For this program, you will tell Tina the Turtle to draw 
-a pentagon.
+a triangle.
 
-You should look at the previous program, 02_Meet_TIna.py
+You should look at the previous program, 02_Meet_Tina.py
 to see how to use the turtle commands.
 
 
@@ -11,12 +11,12 @@ to see how to use the turtle commands.
 # These lines are needed in most turtle programs
 import turtle                           # Tell Python we want to work with the turtle
 turtle.setup (width=600, height=600)    # Set the size of the window
+
 tina = turtle.Turtle()                  # Create a turtle named tina
 
-# Use tina.forward() and tina.left() to draw a pentagon
-# Make each side of the pentagon a different color with 
+# Use tina.forward() and tina.left() to draw a triangle
+# Make each side of the triangle a different color with 
 # tina.pencolor()
-
 
 ... # Your code here
 
