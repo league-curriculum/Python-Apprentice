25e805211e762230288cd2dfc484ffa81339dd7a
Eric Busboom <eric@civicknowledge.com>
2025-03-03 22:18:05 -0800

    Remove obsolete syllabus completion file and update Python version in Turtle lessons

--- lessons/00_Turtles/02_Meet_Tina.py
+++ lessons/00_Turtles/02_Meet_Tina.py
@@ -5,7 +5,7 @@ lines inside the three quotes are also comments, but of a different sort (
 called "doc comments" ) Comments are used to explain what the code does. Read
 the program and try to understand what each line does.
 
-RUM ME! YOu can run this program by clicking on ▶️ icon ar the top of the editor
+RUN ME! YOu can run this program by clicking on ▶️ icon ar the top of the editor
 window.
 
 """
