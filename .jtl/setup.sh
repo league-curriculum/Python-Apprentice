c45bd9dd8b82926cae0084a156c68968f3255243
Eric Busboom <eric@civicknowledge.com>
2025-05-10 21:58:59 -0700

    Update devcontainer image version, refine setup script, and enhance requirements

--- .jtl/setup.sh
+++ .jtl/setup.sh
@@ -12,26 +12,12 @@ echo "THIS_DIR: $THIS_DIR"
 
 cd $TARGET_DIR
 
-# Create a virtual environment and install the requirements.
-(
-    cd $TARGET_DIR && \
-    python -mvenv .venv && \
-    source .venv/bin/activate && \
-    python -mpip install --upgrade pip && \
-    pip3 install  -r $THIS_DIR/requirements.txt
-)
 
-# Git will ask how you want to merge when you pull, 
-# and it's confusing to students. 
-git config --global pull.rebase true
+pip install -r requirements.txt
 
 # Put a newline in the prompt to make it easier to read.
 echo "export PYTHONPATH=$(pwd)/.lib/:$PYTHONPATH" >> ~/.bashrc
-echo 'export PS1="${PS1}\n$ "' >> ~/.bashrc
 
-# Install workspace settings. 
-# cp $THIS_DIR/settings-student.json $TARGET_DIR/.vscode/settings.json
-# This is now done in the League VSCode extension.4
 
 # Fetch the syllabus extension to /vscode/extensionsCache/
 mkdir -p /vscode/extensionsCache/
